713caf33913b8d4f0c12aa4eb60114b67845b01e
bochux <georgehu@xingtangs-MacBook-Pro.local>
2016-08-29 20:15:12 -0700

    fix

--- config.js
+++ config.js
@@ -5,9 +5,11 @@
 //wx766cfa35eaf8f41e
 //appsecret
 //235ee17af796754c65c61dda1504cea1
+
+//wx20b998c53ec523bc
 var config = {
     token: 'mathjoy',
-    appid: 'wx766cfa35eaf8f41e',
+    appid: 'wx20b998c53ec523bc',
     encodingAESKey: '7mCyE0S1B2HzDnrkq3TBBTe19SSU1TllEyzd63gt1Sm'
 };
 
