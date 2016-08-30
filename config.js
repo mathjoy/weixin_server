1dad38b8abf7a5d88116181624d2ce73d7e818b3
bochux <georgehu@xingtangs-MacBook-Pro.local>
2016-08-29 20:01:45 -0700

    fix

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
+
+//http://123.206.228.93
+
+//appID
+//wx766cfa35eaf8f41e
+//appsecret
+//235ee17af796754c65c61dda1504cea1
 var config = {
     token: 'mathjoy',
     appid: 'wx766cfa35eaf8f41e',
-    encodingAESKey: 'EA5aE5YU5VCQ5khHGlOwUxLh3IFmETzYFI1gfLQ83JD'
+    encodingAESKey: '7mCyE0S1B2HzDnrkq3TBBTe19SSU1TllEyzd63gt1Sm'
 };
 
 module.exports = config;
